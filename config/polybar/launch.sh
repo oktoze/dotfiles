b25317ce3682fce9281e5541d5a8bc8b261cb2f6
Kamyab Taghizadeh <kamyab.zad@gmail.com>
2021-05-13 00:39:50 +0430

    Fixed corrupt polybar/launch.sh

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-dir = "$HOME/.config/polybar"
-themes = (`ls --hide="launch.sh" $dir`)
+dir="$HOME/.config/polybar"
 
 launch_bar() {
 	# Terminate already running bar instances
-    killall - q polybar
+    killall -q polybar
 
 	# Wait until the processes have been shut down
-    while pgrep - u $UID - x polybar > /dev/null; do sleep 1; done
+    while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 	# Launch the bar
     if type "xrandr"; then
-    for m in $(xrandr--query | grep " connected" | cut - d" " - f1); do
-        MONITOR = $m polybar - q main - c "$dir/forest/config.ini" &
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -q main -c "$dir/forest/config.ini" &
             done
 		else
-    polybar - q main - c "$dir/forest/config.ini" &
+    polybar -q main -c "$dir/forest/config.ini" &
         fi
 }
+
+
+launch_bar
