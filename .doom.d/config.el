d580ab9e69c0a0a5d71b5d83bd9e36c6d088e085
Kamyab Taghizadeh <kamyabzad@gmail.com>
2021-10-19 09:26:14 +0330

    Adding doom emacs configurations

---
+++ .doom.d/config.el
@@ -0,0 +1,158 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Kamyab Taghizadeh"
+      user-mail-address "kamyab.zad@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font "JetBrains Mono 12"
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;; font for arabic characters
+(set-fontset-font
+   "fontset-default"
+   (cons (decode-char 'ucs #x0600) (decode-char 'ucs #x06ff)) ; arabic
+   "Vazir Code")
+
+;; make underlines a part of 'word'
+(modify-syntax-entry ?_ "w")
+
+;; Org-mode config
+(after! org
+  (setq org-ellipsis " ▼")
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "IN_PROGRESS(p)" "REPEAT(r)" "|" "DONE(d)" "CANCELED(c)")))
+  (setq org-tag-alist '(("PERSONAL" . ?p) ("CODING" . ?c) ("PROJECTS" . ?h) ("EDUCATIONAL" . ?u)
+        ("ENTERTAINMENT" . ?e) ("BOOKS" . ?b) ("MOVIES" . ?m) ("COURSES" . ?r) ("SKILLS" . ?s))))
+
+;; (add-hook! 'org-mode-hook lambda ()
+;;   (display-line-numbers-mode 0)
+;;   (visual-fill-column-mode 1)
+;;   (hl-line-mode 0)
+;;   (org-bullets-mode 1)
+;;   (setq visual-fill-column-width 100
+;;         visual-fill-column-center-text t))
+
+
+(after! company
+  (setq company-dabbrev-downcase 0)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-async-wait 0.01)
+  (setq company-async-timeout 1)
+  (setq company-tooltip-limit 5))
+
+;; (add-hook! 'python-mode-hook lambda ()
+;;   ((tree-sitter-mode 1)
+;;    (tree-sitter-hl-mode 1)
+;;    (python-black-on-save-mode-enable-dwim)))
+
+(add-hook! 'projectile-before-switch-project-hook 'enable-venv-from-pyright)
+
+(after! dap-mode
+  (require 'dap-python))
+
+;; general keymaps
+(define-key evil-normal-state-map (kbd "C-a") 'move-beginning-of-line)
+(define-key evil-visual-state-map (kbd "C-a") 'move-beginning-of-line)
+(define-key evil-motion-state-map (kbd "C-a") 'move-beginning-of-line)
+(define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
+(define-key evil-visual-state-map (kbd "C-e") 'move-end-of-line)
+(define-key evil-motion-state-map (kbd "C-e") 'move-end-of-line)
+(define-key evil-normal-state-map (kbd "C-M-d") 'down-list)
+(define-key evil-visual-state-map (kbd "C-M-d") 'down-list)
+
+(defun evil-go-to-norm-exec ()
+  (interactive)
+  (evil-ex "'<,'>norm "))
+(defun evil-surround-word ()
+  (interactive)
+  (execute-kbd-macro (concat "viwS" (char-to-string (read-char)))))
+
+(define-key evil-visual-state-map (kbd "C-o") 'evil-go-to-norm-exec)
+(define-key evil-normal-state-map (kbd "\"")  'evil-surround-word)
+
+;; Avy keymaps
+(map! :leader
+      :desc "Avy go to word" "a" #'avy-goto-word-0)
+
+(map! :leader
+      (:prefix-map ("d" . "Debug")
+       :desc "Open dap-hydra" "h" #'dap-hydra
+       :desc "Run dap-debug" "d" #'dap-debug-custom
+       :desc "Toggle breakpoint" "b" #'dap-breakpoint-toggle
+       :desc "Delete all breakpoints" "D" #'dap-breakpoint-delete-all
+       :desc "Quit debugging" "q" #'dap-disconnect))
+
+;; calc
+(define-key global-map (kbd "C-x c") #'calc)
+
+(defun dap-debug-custom () (interactive)
+       (if (file-exists-p (concat (projectile-project-root) "manage.py"))
+           (dap-debug (list :type "python"
+                            :args '("runserver" "--noreload")
+                            :cwd (projectile-project-root)
+                            :console "integratedTerminal"
+                            :program (concat (projectile-project-root) "manage.py")
+                            :request "launch"
+                            :name "Django debug"
+                            :debugOptions ["DebugStdLib" "ShowReturnValue" "RedirectOutput"]
+                            :django t))
+         (command-execute 'dap-debug)))
+
+(defun enable-venv-from-pyright ()
+  (if (file-exists-p (concat (projectile-project-root) "pyrightconfig.json"))
+      (when (cdr (assoc 'venv (json-read-file (concat (projectile-project-root) "pyrightconfig.json"))))
+        (pyvenv-workon (cdr (assoc 'venv (json-read-file (concat (projectile-project-root) "pyrightconfig.json"))))))
+    (pyvenv-deactivate)))
